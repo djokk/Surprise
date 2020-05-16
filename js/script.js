ad7f5b2845cfa8ae0b400d71de318e1d24f62750
JAKHONGIR <52883946+djokk@users.noreply.github.com>
2020-05-16 16:26:53 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+	$('.slider').slick({
+		arrows: false,
+		dots: true,
+		infinite: true,
+		speed: 300,
+		slidesToShow: 1,
+		adaptiveHeight: true,
+		autoplay: true,
+		autoplaySpeed: 2500
+	});
+
+	
+})
